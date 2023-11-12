e61253f2a29fa3cc0b79409567320b716a187948
Raj <SoftDevRaj77@gmail.com>
2023-11-12 14:21:18 -0500

    updated controller files

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,9 +1,9 @@
 // controllers/thoughtController.js
 const Thought = require('../models/Thought');
 
-exports.getThoughts = async (req, res) => {
+exports.getAllThoughts = async (req, res) => {
   try {
-    const thoughts = await Thought.find();
+    const thoughts = await Thought.find({}).populate('reactions');
     res.json(thoughts);
   } catch (error) {
     res.status(500).send(error.message);
@@ -12,9 +12,20 @@ exports.getThoughts = async (req, res) => {
 
 exports.createThought = async (req, res) => {
   try {
-    const newThought = new Thought(req.body);
-    const savedThought = await newThought.save();
-    res.status(201).json(savedThought);
+    const newThought = await Thought.create(req.body);
+    res.status(201).json(newThought);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+exports.getThoughtById = async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.thoughtId).populate('reactions');
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.json(thought);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -22,18 +33,56 @@ exports.createThought = async (req, res) => {
 
 exports.updateThought = async (req, res) => {
   try {
-    const updatedThought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updatedThought);
+    const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true });
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.json(thought);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(400).send(error.message);
   }
 };
 
 exports.deleteThought = async (req, res) => {
   try {
-    await Thought.findByIdAndDelete(req.params.id);
-    res.status(200).send('Thought deleted');
+    const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.status(200).send(`Thought ${req.params.thoughtId} deleted`);
   } catch (error) {
     res.status(500).send(error.message);
+  }
+};
+
+exports.addReaction = async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $push: { reactions: req.body } },
+      { new: true, runValidators: true }
+    );
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+exports.removeReaction = async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      { $pull: { reactions: { _id: req.params.reactionId } } },
+      { new: true }
+    );
+    if (!thought) {
+      return res.status(404).send('Thought not found');
+    }
+    res.json(thought);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 // controllers/userController.js
 const User = require('../models/User');
 
-exports.getUser = async (req, res) => {
+exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find({});
     res.json(users);
   } catch (error) {
     res.status(500).send(error.message);
@@ -12,9 +12,20 @@ exports.getUser = async (req, res) => {
 
 exports.createUser = async (req, res) => {
   try {
-    const newUser = new User(req.body);
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -22,18 +33,57 @@ exports.createUser = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
   try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updatedUser);
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(400).send(error.message);
   }
 };
 
 exports.deleteUser = async (req, res) => {
   try {
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).send('User deleted');
+    const user = await User.findByIdAndDelete(req.params.userId);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.status(200).send(`User ${req.params.userId} deleted`);
   } catch (error) {
     res.status(500).send(error.message);
+  }
+};
+
+exports.addFriend = async (req, res) => {
+  try {
+    // $addToSet prevents duplicates
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+exports.removeFriend = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.userId,
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 };
